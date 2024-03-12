4ea5a3c17f879c6cbcb96800085fd0f1ee002331
Diana Ramos <diana.sd.ramos@gmail.com>
2024-03-12 14:58:57 +0000

    user model updated

--- models/User.model.js
+++ models/User.model.js
@@ -18,6 +18,9 @@ const userSchema = new Schema(
       type: String,
       required: [true, "Name is required."],
     },
+    picture: {
+      type: String,
+      default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" },
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
